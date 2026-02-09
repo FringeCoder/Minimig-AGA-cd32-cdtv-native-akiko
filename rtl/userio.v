33c1a02e1e85e621641d979f0ea9b6789cdd3bbf
RobSmithDev <rob@robsmithdev.co.uk>
2026-02-09 20:41:02 +0000

    Catch up to date

--- rtl/userio.v
+++ rtl/userio.v
@@ -55,6 +55,8 @@ module userio
 	output reg  [7:0] memory_config,
 	output reg  [4:0] chipset_config,
 	output reg  [3:0] floppy_config,
+	output reg  [11:0] floppy_ext_drive,
+	output reg        user_port_mode,			// 1=MiSTer Floppy, 0=MT32Pi
 	output reg  [2:0] scanline,
 	output reg  [1:0] ar,
 	output reg  [1:0] blver,
@@ -418,16 +420,20 @@ end
 reg [7:0] cmd;
 
 // reg selects
-wire mem_write_sel    = (cmd[3:0] == 0); // A_A_A_A B,B,... || write system memory, A - 32 bit memory address, B - variable number of bytes
-wire reset_ctrl_sel   = (cmd[3:0] == 1); // XXXXHRBC || reset control   | H - CPU halt, R - reset, B - reset to bootloader, C - reset control block
-wire aud_sel          = (cmd[3:0] == 2);
-wire chip_cfg_sel     = (cmd[3:0] == 3); // XXXGEANT || chipset config  | G - AGA, E - ECS, A - OCS A1000, N - NTSC, T - turbo
-wire cpu_cfg_sel      = (cmd[3:0] == 4); // XXXXKCTT || cpu config      | K - fast kickstart enable, C - CPU cache enable, TT - CPU type (00=68k, 01=68k10, 10=68k20)
-wire memory_cfg_sel   = (cmd[3:0] == 5); // XHFFSSCC || memory config   | H - HRTmon, FF - fast, SS - slow, CC - chip
-wire video_cfg_sel    = (cmd[3:0] == 6); // DDHHLLSS || video config    | DD - dither, HH - hires interp. filter, LL - lowres interp. filter, SS - scanline mode
-wire floppy_cfg_sel   = (cmd[3:0] == 7); // XXXXXFFS || floppy config   | FF - drive number, S - floppy speed
-wire harddisk_cfg_sel = (cmd[3:0] == 8); // XXXXXSMC || harddisk config | S - enable slave HDD, M - enable master HDD, C - enable HDD controler
-wire joystick_cfg_sel = (cmd[3:0] == 9); // XXXXSMMX || joystick config | S - swap joysticks, MM - dig/analog/cd32
+wire mem_write_sel    	= (cmd[3:0] == 0); // A_A_A_A B,B,... || write system memory, A - 32 bit memory address, B - variable number of bytes
+wire reset_ctrl_sel   	= (cmd[3:0] == 1); // XXXXHRBC || reset control   | H - CPU halt, R - reset, B - reset to bootloader, C - reset control block
+wire aud_sel          	= (cmd[3:0] == 2);
+wire chip_cfg_sel     	= (cmd[3:0] == 3); // XXXGEANT || chipset config  | G - AGA, E - ECS, A - OCS A1000, N - NTSC, T - turbo
+wire cpu_cfg_sel      	= (cmd[3:0] == 4); // XXXXKCTT || cpu config      | K - fast kickstart enable, C - CPU cache enable, TT - CPU type (00=68k, 01=68k10, 10=68k20)
+wire memory_cfg_sel   	= (cmd[3:0] == 5); // XHFFSSCC || memory config   | H - HRTmon, FF - fast, SS - slow, CC - chip
+wire video_cfg_sel    	= (cmd[3:0] == 6); // DDHHLLSS || video config    | DD - dither, HH - hires interp. filter, LL - lowres interp. filter, SS - scanline mode
+wire floppy_cfg_sel   	= (cmd[3:0] == 7); // XXXXXFFS || floppy config   | FF - drive number, S - floppy speed
+wire harddisk_cfg_sel 	= (cmd[3:0] == 8); // XXXXXSMC || harddisk config | S - enable slave HDD, M - enable master HDD, C - enable HDD controler
+wire joystick_cfg_sel 	= (cmd[3:0] == 9); // XXXXSMMX || joystick config | S - swap joysticks, MM - dig/analog/cd32
+wire floppyex01_cfg_sel = (cmd[3:0] == 10);// XXAAABBB || External floppy config, AAA/BBB is drive number (1-4) or 0 for disabled
+wire floppyex23_cfg_sel = (cmd[3:0] == 11);// XXCCCDDD || External floppy config, CCC/DDD is drive number (1-4) or 0 for disabled
+wire userport_cfg_sel   = (cmd[3:0] == 12);// xxxxxxxm || User port mode - 0: MT32pi, 1: MiSTer Floppy
+
 always @(posedge clk) begin
 	reg       has_cmd;
 	reg       mrx;
@@ -462,6 +468,9 @@ always @(posedge clk) begin
 				if (memory_cfg_sel)   t_memory_config <= IO_DIN[7:0];
 				if (video_cfg_sel)    {blver, ar, scanline} <= {IO_DIN[11:8],IO_DIN[2:0]};
 				if (floppy_cfg_sel)   floppy_config <= IO_DIN[3:0];
+				if (floppyex01_cfg_sel) { floppy_ext_drive[5:3], floppy_ext_drive[2:0]}  = IO_DIN[5:0];
+				if (floppyex23_cfg_sel) { floppy_ext_drive[11:9], floppy_ext_drive[8:6]}  = IO_DIN[5:0];
+				if (userport_cfg_sel) user_port_mode = IO_DIN[0];
 				if (harddisk_cfg_sel) t_ide_config <= IO_DIN[5:0];
 				if (joystick_cfg_sel) {joy_swap, cd32pad, joy_ana_en} <= {IO_DIN[3], ~IO_DIN[1] & IO_DIN[2], IO_DIN[1]};
 				if (aud_sel)          aud_mix <= IO_DIN[1:0];
